edeaa1a38420087aa4ecf7e8dc6631238ac9975c
Joshua Smith <joshua01smith@gmail.com>
2020-06-20 19:55:40 -0300

    Add navbar, contact page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 const siteTitle = 'Joshua Smith';
 const siteDescription = 'Personal website of Joshua Smith';
@@ -15,6 +16,21 @@ const Layout: React.FC<{ children: React.ReactNode; home?: boolean }> = ({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>{children}</main>
+      <BottomNav />
+    </div>
+  );
+};
+
+const BottomNav: React.FC = () => {
+  const linkClasses = 'flex flex-1 py-2 px-2 justify-center md:p-4 md:text-xl';
+  return (
+    <div className="bg-black text-white fixed overflow-hidden bottom-0 w-full flex justify-around divide-x divide-white">
+      <Link href="/">
+        <a className={linkClasses}>Home</a>
+      </Link>
+      <Link href="/contact">
+        <a className={linkClasses}>Contact</a>
+      </Link>
     </div>
   );
 };
---
+++ pages/contact.tsx
@@ -0,0 +1,50 @@
+import Layout from '../components/Layout';
+
+const Contact: React.FC = () => {
+  return (
+    <Layout>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-full max-w-md justify-center h-10/12">
+          <form
+            className="shadow-md rounded p-6"
+            name="contact"
+            method="post"
+            data-netlify="true"
+          >
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-bold mb-2" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-32"
+                name="message"
+                id="message"
+              ></textarea>
+            </div>
+            <div className="flex justify-end">
+              <button
+                className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
